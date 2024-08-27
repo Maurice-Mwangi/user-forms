f712e774bbbfc38ae1011df9e2d5037240b6a515
Mwas The Coder <mauricemwangi25@gmail.com>
2024-08-27 14:21:27 +0300

    Intoducing JavaScript

---
+++ user_form.js
@@ -0,0 +1,8 @@
+const username = document.querySelector('#txtUsername');
+const phone = document.querySelector('#txtNumber');
+const email = document.querySelector('#txtEmail');
+const password = document.querySelector('#txtPassword');
+const conPassword = document.querySelector('#txtConPassword');
+
+const signupButton = document.querySelector("#btnSignup");
+
